d5e209b87123ace8ab2b0809ee6ebf3a3997b15d
DuranaiK <195920504+DuranaiKhan@users.noreply.github.com>
2026-01-20 18:24:23 +0400

    added new feature

--- OneDrive/Documents/COE420L1/src/Defualt/Hello.java
+++ OneDrive/Documents/COE420L1/src/Defualt/Hello.java
@@ -5,6 +5,10 @@ public class Hello {
 	public static void main(String[] args) {
 	
 		System.out.println("Hello World");
+		B();
+	}
+	public static void B() {
+		System.out.println("New feature B added");
 	}
 
 }
